37f452dd4837429cab63668ab84bac1c2bd5fdfe
WRIGH7Y <92814197+WRIGH7Y@users.noreply.github.com>
2021-11-09 20:09:25 +0000

    Update qbcore.sql

--- qbcore.sql
+++ qbcore.sql
@@ -553,3 +553,58 @@ INSERT INTO `skills` (`id`, `identifier`, `stamina`, `strength`, `driving`, `sho
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+
+-- CORE MDW
+CREATE TABLE `mdw_reports` (
+  `id` varchar(100) NOT NULL,
+  `incident` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `ongoing` tinyint(1) NOT NULL,
+  `expire` varchar(100) NOT NULL,
+  `created` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `mdw_incidents` (
+  `id` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `data` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `mdw_evidence` (
+  `id` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `mdw_announcements` (
+  `id` int(11) NOT NULL,
+  `announcement` longtext NOT NULL,
+  `date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `mdw_announcements`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `mdw_announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `players`
+ADD (`mdw_image` varchar(1000) COLLATE utf8mb4_bin NOT NULL,
+  `mdw_description` longtext COLLATE utf8mb4_bin NOT NULL,
+  `mdw_alias` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `mdw_staffdata` longtext COLLATE utf8mb4_bin NOT NULL);
+
+CREATE TABLE `evidence_storage` (
+  `id` int(11) NOT NULL,
+  `data` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `evidence_storage`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `evidence_storage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
